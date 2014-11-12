19a5e211707458caedf672fd38cc24e93f47d685
beeglebug <updownleftright@gmail.com>
2014-11-12 23:02:10 +0000

    switched to jetpack control script instead of flame

---
+++ Assets/scripts/Jetpack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jetpack : MonoBehaviour {
+
+	Animator _animator;
+	public bool on = false;
+
+	// Use this for initialization
+	void Start () {
+		GameObject flame = GameObject.Find("Flame");
+		_animator = flame.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(on) {
+			_animator.SetBool("jetpack-on", true);
+		} else {
+			_animator.SetBool("jetpack-on", false);
+		}
+	
+	}
+}
--- Assets/scripts/PlayerInputController.cs
+++ Assets/scripts/PlayerInputController.cs
@@ -5,12 +5,15 @@ public class PlayerInputController : MonoBehaviour
 {
 	public float boostSpeed = 20f;
 	public float horizontalSpeed = 10f;
-	public bool isBoosting = false;
+
+	Jetpack _jetpack;
+			
+	void Awake() {
+		_jetpack = GetComponent<Jetpack>(); 
+	}
 	
 	void FixedUpdate()
-	{		
-		isBoosting = false;
-		
+	{	
 		if( Input.GetKey( KeyCode.RightArrow ) )
 		{
 			rigidbody2D.AddForce(new Vector2(horizontalSpeed, 0));
@@ -33,10 +36,14 @@ public class PlayerInputController : MonoBehaviour
 		if( Input.GetKey( KeyCode.Space ) )
 		{
 			rigidbody2D.AddForce(new Vector2(0, boostSpeed));
-			isBoosting = true;
+			_jetpack.on = true;
+		
+		} else {
+		
+			_jetpack.on = false;
+		
 		}
 		
-		rigidbody2D.velocity = Vector2.ClampMagnitude(rigidbody2D.velocity, 10);
-		
+		rigidbody2D.velocity = Vector2.ClampMagnitude(rigidbody2D.velocity, 10);	
 	}
 }
